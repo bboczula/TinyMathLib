72884782774effac0c4bf3c719cccf45082cdc76
bboczula <boczula.bartosz@gmail.com>
2019-06-19 13:33:20 +0100

    Added unit tests for Vector2D multiplication by scalar

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -71,5 +71,37 @@ namespace UnitTests
 			Assert::AreEqual(result.x, 0.0f);
 			Assert::AreEqual(result.y, 0.0f);
 		}
+		TEST_METHOD(TestVectorMultiplicationByScalar1)
+		{
+			TinyMathLib::Vector2D<float> vector;
+			TinyMathLib::Vector2D<float> result = vector * 2;
+
+			Assert::AreEqual(result.x, 0.0f);
+			Assert::AreEqual(result.y, 0.0f);
+		}
+		TEST_METHOD(TestVectorMultiplicationByScalar2)
+		{
+			TinyMathLib::Vector2D<float> vector(3.14f, 3.14f);
+			TinyMathLib::Vector2D<float> result = vector * 2;
+
+			Assert::AreEqual(result.x, 6.28f);
+			Assert::AreEqual(result.y, 6.28f);
+		}
+		TEST_METHOD(TestVectorMultiplicationByScalar3)
+		{
+			TinyMathLib::Vector2D<float> vector(1.0f, 1.5f);
+			TinyMathLib::Vector2D<float> result = vector * 2;
+
+			Assert::AreEqual(result.x, 2.0f);
+			Assert::AreEqual(result.y, 3.0f);
+		}
+		TEST_METHOD(TestVectorMultiplicationByScalar4)
+		{
+			TinyMathLib::Vector2D<float> vector(100.0f, 50.0f);
+			TinyMathLib::Vector2D<float> result = vector * 0.5f;
+
+			Assert::AreEqual(result.x, 50.0f);
+			Assert::AreEqual(result.y, 25.0f);
+		}
 	};
 }
