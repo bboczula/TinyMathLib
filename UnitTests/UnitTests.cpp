8b90a0fd86d155705c55a894f44c9d1d3fad045f
bboczula <bartosz.boczula@intel.com>
2019-07-25 12:25:22 +0200

    Unit tests for dot product commutativity

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -278,6 +278,15 @@ namespace UnitTests
 
 			Assert::AreEqual(32.499f, S1);
 		}
+		TEST_METHOD(TestDotProductCommutativity1)
+		{
+			TinyMathLib::Vector2<float> V1(3.14f, 6.28f);
+			TinyMathLib::Vector2<float> V2(1.23f, 4.56f);
+			float S1 = V1.dotProduct(V2);
+			float S2 = V2.dotProduct(V1);
+
+			Assert::AreEqual(S1, S2);
+		}
 	};
 
 	TEST_CLASS(GeneralVector3DTesting) 
@@ -472,6 +481,15 @@ namespace UnitTests
 
 			Assert::AreEqual(27.389f, S1);
 		}
+		TEST_METHOD(TestDotProductCommutativity1)
+		{
+			TinyMathLib::Vector3<float> V1(3.14f, 6.28f, 5.11f);
+			TinyMathLib::Vector3<float> V2(1.23f, 4.56f, -1.0f);
+			float S1 = V1.dotProduct(V2);
+			float S2 = V2.dotProduct(V1);
+
+			Assert::AreEqual(S1, S2);
+		}
 	};
 
 	TEST_CLASS(GeneralVector4DTesting)
@@ -656,6 +674,15 @@ namespace UnitTests
 
 			const float TOLERANCE = 0.001f;
 			Assert::AreEqual(-12.9289f, S1, TOLERANCE);
+		}
+		TEST_METHOD(TestDotProductCommutativity1)
+		{
+			TinyMathLib::Vector4<float> V1(3.14f, 6.28f, 5.11f, -5.11f);
+			TinyMathLib::Vector4<float> V2(1.23f, 4.56f, -1.0f, 7.89f);
+			float S1 = V1.dotProduct(V2);
+			float S2 = V2.dotProduct(V1);
+
+			Assert::AreEqual(S1, S2);
 		}
 	};
 
